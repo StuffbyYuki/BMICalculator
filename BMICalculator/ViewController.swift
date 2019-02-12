b672d3b76547159ebdf46806da4d68f1080e33c5
Your Name <you@example.com>
2019-02-12 09:21:17 -0700

    Initial Commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by 掛川優希 on 2/12/19.
+//  Copyright © 2019 Yuki Kakegawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
